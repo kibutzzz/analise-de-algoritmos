253f85ddc9c145a12950ba0d2337eac975043f71
Leonardo Ramos <leonardo.ramos@sap.com>
2022-09-06 21:45:15 -0300

    implement dummy solution

--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -1,102 +1,54 @@
 package solution;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
-import static java.math.BigDecimal.*;
+import static java.math.BigDecimal.ONE;
+import static java.math.RoundingMode.UNNECESSARY;
 
 public class Main {
-
-  private static final List<Map.Entry<BigDecimal, BigDecimal>> list = new ArrayList<>();
-
-  static {
-    list.add(new SimpleEntry<>(valueOf(6), valueOf(8)));
-    list.add(new SimpleEntry<>(valueOf(35), valueOf(49)));
-  }
-
-  public static final BigDecimal TWO = valueOf(2);
-  public static final BigDecimal EIGHT = valueOf(8);
+  private static final BigDecimal TWO = BigDecimal.valueOf(2);
 
   public static void main(String[] args) {
-    var i = calculateFirstValue();
-    var j = calculateSecondValue(i);
-    while (!shouldExit()) {
+    int i = 6;
+    int j = 8;
+    int total = 0;
+    int lastValidI = i;
 
+    while (total < 15) {
       if (validate(i, j)) {
-        addToList(i, j);
-        i = calculateFirstValue();
-        j = calculateSecondValue(i);
+        System.out.printf("(%d, %d)%n", i, j);
+        total++;
+        lastValidI = i;
+      }
+      i++;
+      if (i == j) {
+        j++;
+        i = lastValidI;
       }
 
     }
 
-    print();
   }
 
-  private static void print() {
-    for (int i = 0; i < list.size(); i++) {
-      final var item = list.get(i);
-      System.out.println((i + 1) + "(" + item.getKey() + ", " + item.getValue() + ") ");
-    }
+  private static boolean validate(int first, int second) {
+    final var firstDecimal = BigDecimal.valueOf(first);
+    final var sum1 = sumFirstDecimal(firstDecimal);
+    final var sum2 = sumSecondDecimal(firstDecimal, BigDecimal.valueOf(second));
+
+    return sum1.compareTo(sum2) == 0;
   }
 
-  private static void addToList(BigDecimal i, BigDecimal j) {
-    list.add(new SimpleEntry<>(i, j));
-  }
-
-  private static BigDecimal calculateFirstValue() {
-    final var lastNumber = list.get(list.size() - 1).getKey();
-    final var penultimateNumber = list.get(list.size() - 2).getKey();
-    final var factor = lastNumber.divide(penultimateNumber, MathContext.DECIMAL128);
-    return lastNumber.multiply(factor).divide(ONE, 0, RoundingMode.HALF_DOWN);
-  }
-
-  private static boolean shouldExit() {
-    return list.size() >= 15;
-  }
-
-  private static BigDecimal calculateSecondValue(BigDecimal firstValue) {
-    return firstValue.pow(2)
-        .multiply(EIGHT)
-        .add(ONE)
-        .sqrt(MathContext.DECIMAL32)
-        .subtract(ONE)
-        .divide(TWO, RoundingMode.FLOOR);
-  }
-
-  private static boolean validate(BigDecimal first, BigDecimal second) {
-    final var sum1 = sumFirst(first);
-    final var sum2 = sumSecond(first, second);
-
-    final var isValid = sum1.compareTo(sum2) == 0;
-    if (!isValid) {
-      System.out.printf("{%s -> %s - %s -> %s}\n", first, sum1, second, sum2);
-      return false;
-    }
-
-    return isValid;
-  }
-
-  private static BigDecimal sumSecond(BigDecimal first, BigDecimal second) {
+  private static BigDecimal sumSecondDecimal(BigDecimal first, BigDecimal second) {
     return sum(second).subtract(sum(first));
   }
 
-  private static BigDecimal sumFirst(BigDecimal first) {
-    return sum(first.subtract(BigDecimal.ONE));
+  private static BigDecimal sumFirstDecimal(BigDecimal first) {
+    return sum(first.subtract(ONE));
   }
 
   private static BigDecimal sum(BigDecimal n) {
-
-    if (n.compareTo(BigDecimal.ZERO) <= 0) return BigDecimal.ZERO;
-    return n.multiply(n.add(BigDecimal.ONE))
-        .divide(BigDecimal.valueOf(2));
+    return n.multiply(n.add(ONE)).divide(TWO, UNNECESSARY);
   }
-
 
 }
 
